e91090c3dbcd43ae2118da9e74f50ad024a410c0
Khusboo Kumari <36759104+khush611@users.noreply.github.com>
2020-11-08 11:54:57 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import React, { Component, useState } from "react";
-import "./styles.css";
+import React, { Component, useState, useEffect } from "react";
+import "../styles/App.css";
 
 const App = () => {
   const [renderBall, setRenderBall] = useState(false);
@@ -9,52 +9,51 @@ const App = () => {
     left: "0px",
     top: "0px"
   });
-
-  function buttonClickHandler() {
+  const reset = () => {
+    setX(0);
+    setY(0);
+    setBallPosition({ left: "0px", top: "0px" });
+    setRenderBall(false);
+  };
+  const buttonClickHandler = () => {
     setRenderBall(true);
-    document.addEventListener("keydown", handleKeyPress);
-  }
-  const reset = () => {};
+  };
+  useEffect(() => {
+    const arrowKeyHandler = (event) => {
+      const key = event.keyCode;
+      let curX = x;
+      let curY = y;
+      if (key === 39) {
+        curX += 5;
+        setX(curX);
+      } else if (key === 37) {
+        curX -= 5;
+        setX(curX);
+      } else if (key === 38) {
+        curY -= 5;
+        setY(curY);
+      } else if (key === 40) {
+        curY += 5;
+        setY(curY);
+      }
+      setBallPosition({ left: curX + "px", top: curY + "px" });
+    };
+    document.addEventListener("keydown", arrowKeyHandler);
+    return () => {
+      document.removeEventListener("keydown", arrowKeyHandler);
+    };
+  }, [x, y]);
+
   const renderChoice = () => {
     if (renderBall) {
-      return <div id="b" className="ball" style={ballPosition}></div>;
+      return <div className="ball" style={ballPosition}></div>;
     } else
       return (
-        <button className="start" onClick={buttonClickHandler}>
+        <button onClick={buttonClickHandler} className="start">
           Start
         </button>
       );
   };
-  function handleKeyPress(event) {
-    if (event.keyCode === 39) {
-      handleRight();
-    } else if (event.keyCode === 40) {
-      handleDown();
-    } else if (event.keyCode === 38) {
-      handleUp();
-    } else if (event.keyCode === 37) {
-      handleLeft();
-    }
-  }
-  function handleRight() {
-    var element = document.getElementById("b");
-    element.style.left = parseInt(element.style.left) + 5 + "px";
-    console.log(ballPosition);
-  }
-  function handleLeft() {
-    var element = document.getElementById("b");
-    element.style.left = parseInt(element.style.left) - 5 + "px";
-  }
-
-  function handleUp() {
-    var element = document.getElementById("b");
-    element.style.top = parseInt(element.style.top) - 5 + "px";
-  }
-
-  function handleDown() {
-    var element = document.getElementById("b");
-    element.style.top = parseInt(element.style.top) + 5 + "px";
-  }
 
   return (
     <div className="playground">
